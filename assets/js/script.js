57ea0415371b0dc5d86b4614f52e8602df8a9ff4
Owais Saleem <saleemowais62@gmail.com>
2024-02-20 16:51:06 +0500

    Covid Screening test

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+var answers = [];
+function recordAnswer(qno, ans, nqno) {
+    answers[qno] = ans;
+    if (nqno > 0) {
+        jQuery('.question .row').removeClass("active");
+        jQuery('.q' + nqno).addClass("active");
+    } else {
+
+        q1 = answers[1];
+        q2 = answers[2];
+        q3 = answers[3];
+        q4 = answers[4];
+        q5 = answers[5];
+        q6 = answers[6];
+        q7 = answers[7];
+        q8 = answers[8];
+        q9 = answers[9];
+        q10 = answers[10];
+
+        if ((q1, q2, q4, q8, q9, q6 == 1) && (q3, q5, q7, q10 == 1 || 2)) {
+            jQuery('.question .row').removeClass("active");
+            jQuery('.row.result1').addClass("active");
+
+        }
+
+        else {
+
+            q1 = answers[1];
+            q2 = answers[2];
+            q3 = answers[3];
+            q4 = answers[4];
+            q5 = answers[5];
+            q6 = answers[6];
+            q7 = answers[7];
+            q8 = answers[8];
+            q9 = answers[9];
+            q10 = answers[10];
+
+            if ((q2, q1, q4 == 1) && (q3, q6, q5, q7, q10 == 1 || 2) && (q8, q9 == 1 || 2)) {
+                jQuery('.question .row').removeClass("active");
+                jQuery('.row.result2').addClass("active");
+
+            } else {
+
+                q1 = answers[1];
+                q2 = answers[2];
+                q3 = answers[3];
+                q4 = answers[4];
+                q5 = answers[5];
+                q6 = answers[6];
+                q7 = answers[7];
+                q8 = answers[8];
+                q9 = answers[9];
+                q10 = answers[10];
+
+                if ((q2, q1, q4 != 1) && (q3, q6, q5, q7, q10 != 1 || 2) && (q8, q9 != 1 || 2)) {
+                    jQuery('.question .row').removeClass("active");
+                    jQuery('.row.result3').addClass("active");
+
+                }
+            }
+            }
+
+
+        }
+    }
